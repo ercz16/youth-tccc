77a184df98e28e34c4d9de81301d85ad6ad18826
ercz16 <ericzhou001@gmail.com>
2021-06-30 18:29:48 -0400

    finished site

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome to the index page."
+    return render_template('index.html')
 
-@app.route("/signin")
-def who():
-    return "Who are you?"
+@app.route("/mission")
+def mission():
+    return render_template('mission.html')
+
+@app.route("/resources")
+def resources():
+    return render_template('resources.html')
+
+@app.route("/activities")
+def activities():
+    return render_template('activities.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
